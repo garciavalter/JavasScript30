63fb2d9746f176835c3fff49cc31529ce2137e66
Valter Garcia <junior.opto@gmail.com>
2020-01-10 16:30:01 -0300

    improved for work on mobile.

--- DrumProject/controllers/app.js
+++ DrumProject/controllers/app.js
@@ -11,6 +11,14 @@ window.addEventListener('keydown', e => {
 
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => {
+    key.addEventListener('click', () => {
+        const audio = document.querySelector(`audio[data-key="${key.getAttribute('data-key')}"]`);
+        if(!audio) return;
+        audio.currentTime = 0;
+        audio.play();
+        key.classList.add('playing');
+    });
+
     key.addEventListener('transitionend', function removeTransition(e) {
         if(e.propertyName !== 'transform') return;
         this.classList.remove('playing');
