63cd49800d7cadb8f37a8eba3ac47bfd559368e8
Valter Garcia <junior.opto@gmail.com>
2020-01-15 20:54:47 -0300

    hightlight function working properly.

---
+++ AjaxTypeAhead/sandbox.js
@@ -0,0 +1,33 @@
+const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+const query = document.querySelector('.search');
+const list = document.querySelector('.suggestions');
+
+const cities = [];
+
+fetch(endpoint)
+    .then(blog =>  blog.json())
+    .then(data => cities.push(...data));
+
+function findMatches(wordToMatch, cities){
+    return cities.filter(place => {
+        const regex = new RegExp (wordToMatch, 'gi');
+        return place.city.match(regex) || place.state.match(regex);
+    });
+}
+
+function displayMatches(){
+    const matchArray = findMatches(this.value, cities)
+    const html = matchArray.map(place => {
+        const regex = new RegExp (this.value, 'gi');
+        const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`)
+        return `
+        <li>
+            <span class=name>${cityName}, ${place.state}</span>
+            <span class=population>${place.population}</span>
+        </li>`
+    }).join('');
+    list.innerHTML = html;
+}
+
+query.addEventListener('change', displayMatches);
+query.addEventListener('keyup', displayMatches);
