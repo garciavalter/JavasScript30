86dccdc5afccd47b9756d66d6a235c33bb537198
Valter Garcia <junior.opto@gmail.com>
2020-01-20 21:41:03 -0300

    checking holding shift working, need uncheck holding shift.

--- HoldShiftMultipleCheckbox/sandbox.js
+++ HoldShiftMultipleCheckbox/sandbox.js
@@ -1,1 +1,27 @@
-cd
+const checkboxes = document.querySelectorAll('.inbox input[type = checkbox]');
+console.log(checkboxes);
+checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('click', handleCheck);
+});
+
+let lastChecked;
+
+function handleCheck(e) {
+    if (e.shiftKey && this.checked){
+        let status = false
+        checkboxes.forEach(checkbox => {
+            if (checkbox == lastChecked) {
+                status = !status; 
+                }
+            if (checkbox == this) {
+                status = !status;
+            } else {
+                checkbox.checked = status;
+            }            
+        })
+    }
+
+    
+    lastChecked = this;
+    console.log(e);
+}
